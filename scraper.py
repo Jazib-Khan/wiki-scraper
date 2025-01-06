cb8a4ef3a92de196808c23e5414c50d6d37f6b12
Jazib-Khan <jazib1khan@gmail.com>
2025-01-06 00:23:38 +0000

    Scraping title from id

--- scraper.py
+++ scraper.py
@@ -1,6 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 
 response = requests.get(
     url="https://en.wikipedia.org/wiki/web_scraping",
 )
-print(response.status_code)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+title = soup.find(id="firstHeading")
+print(title.string)
