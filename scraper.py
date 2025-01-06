f559f4e90fa8eb6e6259e4aec8198789ee5ceda8
Jazib-Khan <jazib1khan@gmail.com>
2025-01-06 00:41:29 +0000

    Fetches wiki article, extracts title, finds random link and then calls function again

--- scraper.py
+++ scraper.py
@@ -2,26 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 import random
 
-response = requests.get(
-    url="https://en.wikipedia.org/wiki/web_scraping",
-)
-soup = BeautifulSoup(response.content, 'html.parser')
+def scrapeWikiArticle(url):
+    response = requests.get(
+        url=url,
+    )
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-title = soup.find(id="firstHeading")
-print(title.string)
+    title = soup.find(id="firstHeading")
+    print(title.string)
 
 # Get all the links
-allLinks = soup.find(id="bodyContent").find_all("a")
-random.shuffle(allLinks)
-linktoScrape = 0
+    allLinks = soup.find(id="bodyContent").find_all("a")
+    random.shuffle(allLinks)
+    linktoScrape = 0
 
-for link in allLinks:
-    # We are only interested in other wiki articles
-    if link['href'].find("/wiki/") == -1:
-        continue
+    for link in allLinks:
+        # We are only interested in other wiki articles
+        if link['href'].find("/wiki/") == -1:
+            continue
 
-    # Use this link to scrape
-    linkToScrape = link
-    break
+        # Use this link to scrape
+        linkToScrape = link
+        break
 
-print(linkToScrape)
+    scrapeWikiArticle("https://en.wikipedia.org" + linkToScrape['href'])
+
+scrapeWikiArticle("https://en.wikipedia.org/wiki/Food")
